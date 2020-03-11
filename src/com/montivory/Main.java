05f9369299db34c9493dc73261a550d307080ecc
kamolpatmontivory <kamolpat.k@montivory.com>
2020-03-11 15:38:50 +0700

    Program for calculate grade (base)

---
+++ src/com/montivory/Main.java
@@ -0,0 +1,27 @@
+package com.montivory;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please Input your score to grade calculate: ");
+        int score = sc.nextInt();
+
+        if (score >= 0 && score <= 49) {
+            System.out.println("Your grade is F");
+        } else if (score >= 50 && score <= 59) {
+            System.out.println("Your grade is D");
+        } else if (score >= 60 && score <= 69) {
+            System.out.println("Your grade is C");
+        } else if (score >= 70 && score <= 79) {
+            System.out.println("Your grade is B");
+        } else if (score >= 80 && score <= 100) {
+            System.out.println("Your grade is A");
+        } else {
+            System.out.println("your score is invalid number");
+        }
+    }
+}
