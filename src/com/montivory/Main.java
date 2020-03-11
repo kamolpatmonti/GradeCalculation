da29858bc9baec12ba1917ba2226f702687517f2
kamolpatmontivory <kamolpat.k@montivory.com>
2020-03-11 15:50:43 +0700

    update range score and grade

--- src/com/montivory/Main.java
+++ src/com/montivory/Main.java
@@ -12,12 +12,18 @@ public class Main {
 
         if (score >= 0 && score <= 49) {
             System.out.println("Your grade is F");
-        } else if (score >= 50 && score <= 59) {
-            System.out.println("Your grade is D");
-        } else if (score >= 60 && score <= 69) {
+        } else if (score >= 50 && score <= 54) {
+            System.out.println("Your grade is D+");
+        } else if (score >= 55 && score <= 59) {
+            System.out.println("Your grade is D+");
+        } else if (score >= 60 && score <= 64) {
             System.out.println("Your grade is C");
-        } else if (score >= 70 && score <= 79) {
+        } else if (score >= 65 && score <= 69) {
+            System.out.println("Your grade is C+");
+        } else if (score >= 70 && score <= 74) {
             System.out.println("Your grade is B");
+        } else if (score >= 75 && score <= 79) {
+            System.out.println("Your grade is B+");
         } else if (score >= 80 && score <= 100) {
             System.out.println("Your grade is A");
         } else {
